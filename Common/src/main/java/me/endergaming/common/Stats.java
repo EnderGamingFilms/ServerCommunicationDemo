ae2b5bed54a71cf31d6e6087a5b0f702dd2566e1
EnderGamingFilms <endergamingfilms@gmail.com>
2022-10-01 11:54:17 -0700

    feat: file saving & type adapter

--- Common/src/main/java/me/endergaming/common/Stats.java
+++ Common/src/main/java/me/endergaming/common/Stats.java
@@ -1,9 +1,13 @@
 package me.endergaming.common;
 
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,5 +27,93 @@ public class Stats {
         int total = this.killsMap.getOrDefault(type, 0) + amount;
 
         this.killsMap.put(type, total);
+    }
+
+    public static class StatsAdapter extends TypeAdapter<Stats> {
+
+        @Override
+        public void write(JsonWriter out, Stats value) throws IOException {
+            out.beginObject();
+
+            out.name("killsMap");
+            out.beginObject();
+            for (var entry : value.killsMap.entrySet()) {
+                out.name(entry.getKey());
+                out.value(entry.getValue());
+            }
+            out.endObject();
+
+            out.name("joins");
+            out.value(value.joins);
+
+            out.name("damageDealt");
+            out.value(value.damageDealt);
+
+            out.name("damageTaken");
+            out.value(value.damageTaken);
+
+            out.name("blocksMined");
+            out.value(value.blocksMined);
+
+            out.name("blocksPlaced");
+            out.value(value.blocksPlaced);
+
+            out.name("deaths");
+            out.value(value.deaths);
+
+            out.name("itemsDropped");
+            out.value(value.itemsDropped);
+
+            out.endObject();
+        }
+
+        @Override
+        public Stats read(JsonReader in) throws IOException {
+            Stats stats = new Stats();
+
+            in.beginObject();
+
+            while (in.hasNext()) {
+                String name = in.nextName();
+
+                switch (name) {
+                    case "killsMap":
+                        in.beginObject();
+                        while (in.hasNext()) {
+                            String type = in.nextName();
+                            int amount = in.nextInt();
+
+                            stats.addKill(type, amount);
+                        }
+                        in.endObject();
+                        break;
+                    case "joins":
+                        stats.joins = in.nextInt();
+                        break;
+                    case "damageDealt":
+                        stats.damageDealt = in.nextDouble();
+                        break;
+                    case "damageTaken":
+                        stats.damageTaken = in.nextDouble();
+                        break;
+                    case "blocksMined":
+                        stats.blocksMined = in.nextInt();
+                        break;
+                    case "blocksPlaced":
+                        stats.blocksPlaced = in.nextInt();
+                        break;
+                    case "deaths":
+                        stats.deaths = in.nextInt();
+                        break;
+                    case "itemsDropped":
+                        stats.itemsDropped = in.nextInt();
+                        break;
+                }
+            }
+
+            in.endObject();
+
+            return stats;
+        }
     }
 }
--- Communicator/src/main/java/me/endergaming/communicator/Communicator.java
+++ Communicator/src/main/java/me/endergaming/communicator/Communicator.java
@@ -3,6 +3,10 @@ package me.endergaming.communicator;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import lombok.SneakyThrows;
@@ -21,6 +25,14 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.util.UUID;
 import java.util.logging.Level;
 
@@ -32,6 +44,11 @@ public final class Communicator extends JavaPlugin implements Listener {
             return new Stats();
         }
     });
+    private final Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .enableComplexMapKeySerialization()
+            .registerTypeAdapter(Stats.class, new Stats.StatsAdapter())
+            .create();
 
     Server server;
 
@@ -50,12 +67,82 @@ public final class Communicator extends JavaPlugin implements Listener {
         this.server.start();
 
         this.getLogger().log(Level.INFO, "Server started on port 3009");
+
+        this.read();
+    }
+
+    public void read() {
+        try {
+            File file = this.getDataFile();
+
+            Reader reader = new BufferedReader(new FileReader(file));
+
+            JsonArray _array = this.gson.fromJson(reader, JsonArray.class);
+
+            if (_array == null) {
+                return;
+            }
+
+            for (var _element : _array) {
+                JsonObject _stats = _element.getAsJsonObject();
+
+                String id = _stats.get("uuid").getAsString();
+
+                if (id.isBlank()) {
+                    continue;
+                }
+
+                UUID uuid = UUID.fromString(id);
+                Stats stats = this.gson.fromJson(_stats.get("stats"), Stats.class);
+
+                this.dataCache.put(uuid, stats);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File getDataFile() throws IOException {
+        File file = new File(this.getDataFolder(), "data.json");
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+
+        return file;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         HandlerList.unregisterAll((Listener) this);
+
+        try {
+            File file = this.getDataFile();
+
+            Writer writer = new BufferedWriter(new FileWriter(file));
+
+            JsonArray _array = new JsonArray();
+
+            for (var entry : this.dataCache.asMap().entrySet()) {
+                JsonObject _stats = new JsonObject();
+
+                _stats.addProperty("uuid", entry.getKey().toString());
+                _stats.add("stats", this.gson.toJsonTree(entry.getValue()));
+
+                _array.add(_stats);
+            }
+
+            this.gson.toJson(_array, writer);
+
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         this.server.shutdown();
     }
